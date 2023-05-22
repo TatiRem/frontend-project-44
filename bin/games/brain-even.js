47a9940beb5e5a92120eb663ebbc7a6ae249dacb
tati <remigi_tati@mail.ru>
2023-05-23 02:13:02 +0300

    removed game description files from the bin directory

--- bin/games/brain-even.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-
-import { threeStagesGame, task } from '../../src/index.js';
-import nameRequest from '../../src/cli.js';
-
-const userName = nameRequest();
-const nameGame = 'even';
-task(nameGame);
-console.log(threeStagesGame(userName, nameGame));
--- bin/games/brain-prime.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-
-import { threeStagesGame, task } from '../../src/index.js';
-import nameRequest from '../../src/cli.js';
-
-const userName = nameRequest();
-const nameGame = 'prime';
-task(nameGame);
-console.log(threeStagesGame(userName, nameGame));
